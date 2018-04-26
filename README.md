44ebc372f8249a22fc6183d1b57fef70014e5b46
Thomas Deneulin <thomas@weglot.com>
2018-04-26 17:00:11 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,4 @@
+<!-- logo -->
+<img src="https://cdn.weglot.com/logo/logo-hor.png" height="40" />
+
+# Drupal module Translate
---
+++ weglot.js
@@ -0,0 +1,22 @@
+(function($) {
+
+    $(document).ready(function() {
+        $("input[name=api_key]").blur(function() {
+            var key = $(this).val();
+            $.ajax({
+                dataType: "json",
+                url:
+                    "https://weglot.com/api/user-info?api_key=" +
+                    key,
+                success: function(data) {
+                    $(".wg-keyres").remove();
+                    $("input[name=api_key]").after('<span class="wg-keyres wg-okkey"></span>');
+                },
+                error :function(data){
+                    $(".wg-keyres").remove();
+                    $("input[name=api_key]").after('<span class="wg-keyres wg-nokkey"></span>');
+                }
+            });
+        });
+    });
+})(jQuery);
